e25f566da5460c6a70aa7cb8d4246f47501f79f4
Pravnat <91091738+Pravnat@users.noreply.github.com>
2021-11-03 10:50:25 +0530

    fbtask

---
+++ Facebook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fbask
+{
+    class Facebook
+    {
+        string Fname;
+        string Lastname;
+        string email;
+        long mobnum;
+        DateTime dob;
+
+        public Facebook()
+        {
+        }
+
+        public Facebook(string fname1, string lastname1, string email, long mobnum, DateTime dob)
+        {
+            Fname1 = fname1;
+            Lastname1 = lastname1;
+            Email = email;
+            Mobnum = mobnum;
+            Dob = dob;
+        }
+
+        public string Fname1 { get => Fname; set => Fname = value; }
+        public string Lastname1 { get => Lastname; set => Lastname = value; }
+        public string Email { get => email; set => email = value; }
+        public long Mobnum { get => mobnum; set => mobnum = value; }
+        public DateTime Dob { get => dob; set => dob = value; }
+
+        public override bool Equals(object obj)
+        {
+            Facebook fb = (Facebook)obj;
+            return this.Email.Equals(fb.Email) && this.Mobnum.Equals(fb.Mobnum);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,15},{1,15},{2,15},{3,15}", Fname + Lastname1, Email, Mobnum, Dob);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fbask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fb_user_acc;
+            Facebook[] fb_users;
+            Console.WriteLine("Enter the no of facebook :");
+            fb_user_acc = int.Parse(Console.ReadLine());
+            fb_users = new Facebook[fb_user_acc];
+            for(int i =0;i< fb_user_acc; i++)
+            {
+                string u_Fname;
+                string u_Lastname;
+                string u_email;
+                long u_mobnum;
+                DateTime u_dob;
+
+                Console.WriteLine("enter fname");
+                u_Fname = Console.ReadLine();
+                Console.WriteLine("enter lname");
+                u_Lastname = Console.ReadLine();
+                Console.WriteLine("enter email");
+                u_email = Console.ReadLine();
+                Console.WriteLine("enter mobile number");
+                u_mobnum =long.Parse( Console.ReadLine());
+                Console.WriteLine("enter dob");
+                u_dob = Convert.ToDateTime(Console.ReadLine());
+                Facebook fb = new Facebook(u_Fname, u_Lastname, u_email, u_mobnum, u_dob);
+                if (!fb_users.Contains(fb))
+                {
+                    fb_users[i] = fb;
+                }
+                else
+                {
+                    Console.WriteLine("account is exist");
+                    i--;
+                }
+            }
+        }
+    }
+}
